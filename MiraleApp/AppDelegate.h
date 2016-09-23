ee0db70690cb8e6d7b79272d6cc40242c53f5ced
Evgen Bakumenko <yeba@ptua-wm0165.local>
2016-09-23 22:27:19 +0300

    Initial Commit

---
+++ MiraleApp/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  MiraleApp
+//
+//  Created by YeBa on 9/23/16.
+//  Copyright © 2016 YeBa. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ MiraleApp/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  MiraleApp
+//
+//  Created by YeBa on 9/23/16.
+//  Copyright © 2016 YeBa. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
